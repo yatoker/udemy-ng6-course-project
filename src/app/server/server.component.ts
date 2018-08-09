e3e913a0aecedf065ac60a90fe5c3ca3a9eefa45
Yusuf Andre Toker <yusufandre@gamil.com>
2018-08-09 13:39:26 +0300

    ngClass Attribute directive sample

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: ['.online{ color: white; }']
 })
 export class ServerComponent {
     serverID: number = 10;
