2d1f20c5f679456ef166bee654dae46d24466f38
Yusuf Andre Toker <yusufandre@gamil.com>
2018-08-09 09:32:59 +0300

    Forms Module added for ngModel directive to work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -17,6 +18,7 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
